8d42dab4943c7b3b8bc7f5ebee0166612981d264
edmilinski <edmilinski@users.noreply.github.com>
2017-09-27 07:40:54 +1000

    Add files via upload

---
+++ EM_Webapi/SQL/Configure.sql
@@ -0,0 +1,4 @@
+﻿CREATE LOGIN [ed] WITH PASSWORD = 'Password1.';
+use [EMNotes]
+CREATE USER [ed] FOR LOGIN [ed];
+exec sp_addrolemember 'db_owner', 'ed'
---
+++ EM_Webapi/SQL/Todos.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Todos] (
+    [id]       INT        IDENTITY (1, 1) NOT NULL,
+    [modified] DATETIME   DEFAULT (getdate()) NOT NULL,
+	[userid]  VARCHAR (50)  NULL,
+	[title] VARCHAR (100)  NULL,
+	[tags]  VARCHAR (100)  NULL,
+    [content]  VARCHAR (2000),
+	[urgent]  bit DEFAULT 0 NOT NULL ,
+	[completed]  bit DEFAULT 0 NOT NULL ,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+go
